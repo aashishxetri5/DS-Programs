9c2a756774ca2ebdc0a22df2064bfeea7b1a768b
Aashish Katwal <aashiskutwal5@gmail.com>
2022-05-21 16:29:08 +0545

    uploading lab II programs

---
+++ Lab II/Q6.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int gcdExtended(int a, int b, int* x, int* y)
+{
+	// Base Case
+	if (a == 0) {
+		*x = 0;
+		*y = 1;
+		return b;
+	}
+
+	int x1, y1; 
+	int gcd = gcdExtended(b % a, a, &x1, &y1);
+
+	// Update x and y using results of recursive call
+	*x = y1 - (b / a) * x1;
+	*y = x1;
+
+	return gcd;
+}
+
+int main(){
+
+    int a1, a2, m1, m2, M;
+    int temp, y1, y2;
+
+    printf("Enter a1 and a2: ");
+    scanf("%d %d", &a1, &a2);
+    printf("Enter m1 and m2: ");
+    scanf("%d %d", &m2, &m2);
+
+    M = m1 * m2;
+    temp = m1;
+    m1 = m2; //M/m1
+    m2 = temp; //M/m2
+
+    temp = gcdExtended(m1, m2, &y1, &y2);
+    temp = ((m1 * y1 * a1) + (m2 * y2 * a2)) % M;
+    printf("X = %d", temp);
+    
+    return 0;
+
+}
